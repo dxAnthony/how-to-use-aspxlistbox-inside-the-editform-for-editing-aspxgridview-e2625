ba07406130c827b6769aa26250c6df667ba71bbc
Anton Sarmin <anton.sarmin@devexpress.com>
2018-05-11 17:28:46 +0300

    test sample

---
+++ CS/test.ts
@@ -0,0 +1,16 @@
+﻿import { Selector } from 'testcafe';
+
+fixture`Getting Started`
+    .page`https://codecentral.devexpress.com/128543494/`
+
+test('My first test', async t => {
+    await t
+        .click("#ASPxGridView1_DXCBtn3") //start editing;
+
+    const listBoxRowsCount = Selector(".dxeListBoxItemRow_DevEx").count;
+    const selectedNodesCount = Selector(".dxWeb_edtCheckBoxChecked_DevEx").count;
+
+    await t
+        .expect(listBoxRowsCount).gt(1)
+        .expect(selectedNodesCount).eql(2);
+});
